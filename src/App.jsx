c05f44a31149ba0b907ecac6fcfd95f16072d967
Mohammad Nandurbarwala <mohammdali0326@gmail.com>
2025-02-22 10:09:02 +0530

    initialized git

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="bg-purple-500">
+        <h1 className="text-xl">Hello, DaisyUI</h1>
+        <button className="btn">Hello daisyUI</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
