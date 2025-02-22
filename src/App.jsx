ddf7a12b4dad77db54e22382a7c433aa27817ed7
Your Name <mohammadali0326@gmail.com>
2025-02-22 12:47:47 +0530

    created Home, about & notfound pages, setup notfound pages with content. added pages routes in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Layout/Navbar";
+import Footer from "./Components/Layout/Footer";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="flex flex-col justify-between h-screen">
         <Navbar title="Github Finder" />
-        <main>
+        <main className="container mx-auto px-3 pb-12">
           <Routes>
-            <Route path="/" />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/*" element={<NotFound />} />
+            <Route path="/notFound" element={<NotFound />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </BrowserRouter>
   );
