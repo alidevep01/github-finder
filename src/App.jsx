901bcb04d6b1f8b25e65045c92485f9e308b3b9d
Your Name <mohammadali0326@gmail.com>
2025-03-07 13:21:11 +0530

    created useContext hook for github api and imported in userResults and APP.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,26 @@ import Footer from "./Components/Layout/Footer";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import NotFound from "./Pages/NotFound";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar title="Github Finder" />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/*" element={<NotFound />} />
-            <Route path="/notFound" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <GithubProvider>
+      <BrowserRouter>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar title="Github Finder" />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/*" element={<NotFound />} />
+              <Route path="/notFound" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </GithubProvider>
   );
 }
 
